5142345a6b195412ccc7a9fe63ab6d1d48f28906
ChiragAgg5k <chiragaggarwal5k@gmail.com>
2023-06-12 16:35:57 +0530

    added formsubmit

--- src/app/ContactMe.tsx
+++ src/app/ContactMe.tsx
@@ -29,29 +29,43 @@ export default function ContactMe() {
 				</span>
 			</div>
 
-			<div className="flex w-full flex-col px-10 py-5 pt-10 sm:w-1/2 sm:px-0 sm:pl-10">
+			<form
+				className="flex w-full flex-col px-10 py-5 pt-10 sm:w-1/2 sm:px-0 sm:pl-10"
+				action={
+					"https://formsubmit.co/5ace9e3323a2cac0fc07db778c9567fa"
+				}
+				method="POST">
 				<input
 					type="text"
 					placeholder="Your Name"
+					name="Name"
+					required
 					className="mb-2 h-10 max-w-xl rounded-lg border-2 bg-white p-5 text-sm hover:border-cyan-500 dark:border-transparent dark:bg-gray-900 dark:hover:border-gray-700"
 				/>
 				<input
-					type="text"
+					type="email"
 					placeholder="Your Email"
+					name="Email"
+					required
 					className="mb-2 h-10 max-w-xl rounded-lg border-2 bg-white p-5 text-sm hover:border-cyan-500 dark:border-transparent dark:bg-gray-900 dark:hover:border-gray-700"
 				/>
 				<textarea
 					rows={5}
 					placeholder="Your Message"
+					name="Message"
+					minLength={10}
+					required
 					className="max-w-xl rounded-lg border-2 bg-white p-5 text-sm hover:border-cyan-500 dark:border-transparent dark:bg-gray-900 dark:hover:border-gray-700"
 				/>
-				<button className="btn group relative mt-5 inline-flex h-12 w-40 items-center justify-start overflow-hidden rounded-lg border-2 bg-white text-sm transition-all dark:border-0 dark:bg-gray-900">
+				<button
+					type="submit"
+					className="btn group relative mt-5 inline-flex h-12 w-40 items-center justify-start overflow-hidden rounded-lg border-2 bg-white text-sm transition-all dark:border-0 dark:bg-gray-900">
 					<span className="-z-1 absolute left-0 top-0 h-0 w-0 rounded bg-cyan-600 transition-all duration-500 ease-out group-hover:h-full group-hover:w-full dark:bg-cyan-700"></span>{" "}
 					<span className="z-10 w-full text-gray-400 transition-colors duration-300 ease-in-out group-hover:text-white">
 						Submit
 					</span>
 				</button>
-			</div>
+			</form>
 		</div>
 	);
 }
