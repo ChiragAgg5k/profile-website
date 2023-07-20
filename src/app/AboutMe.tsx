7e142f95adc3f53c1bb3d9aa9571e9cdf70b2c78
ChiragAgg5k <chiragaggarwal5k@gmail.com>
2023-07-20 12:01:05 +0530

    changed pfp size

--- src/app/AboutMe.tsx
+++ src/app/AboutMe.tsx
@@ -12,7 +12,7 @@ export default function AboutMe() {
           alt="Profile Picture"
           width={480}
           height={480}
-          className="mb-10 w-full max-w-xs rounded-full border-4 border-cyan-500 grayscale filter transition duration-300 hover:filter-none dark:border-cyan-700 md:mb-0"
+          className="mb-10 w-full max-w-[280px] rounded-full border-4 border-cyan-500 grayscale filter transition duration-300 hover:filter-none dark:border-cyan-700 md:mb-0"
         />
         <p className="flex w-full max-w-2xl items-center text-center text-base md:mb-0 md:ml-10 md:text-lg">
           {`I'm a student at Bennett University in Greater Noida, India, where I'm studying a Bachelor of Technology in Computer Science Engineering. I am specializing in artificial intelligence, but I'll experiment with any new technology I come across. Details about my schooling, abilities, and completed projects are provided here. I hope you enjoy them, and please consider emailing me any comments you may have.`}
