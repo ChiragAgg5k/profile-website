5400bd6b3fa2f232b8622d1bfd4e81d619627a2a
ChiragAgg5k <chiragaggarwal5k@gmail.com>
2024-09-24 02:10:47 +0530

    added more blogs

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -10,7 +10,7 @@ export const metadata = {
 const posts = [
   {
     title: "Mastering npm: A Comprehensive Guide to Package Management",
-    thumbnail: "/npm-guide.png",
+    thumbnail: "/blog/npm-guide.png",
     icon: "📦",
     href: "https://dev.to/chiragagg5k/mastering-npm-a-comprehensive-guide-to-package-management-3h0m",
     publishedAt: "2023-01-01",
@@ -27,7 +27,7 @@ const posts = [
   },
   {
     title: "Conditional Dependency Management Using Maven Profiles",
-    thumbnail: "/conditional-maven.png",
+    thumbnail: "/blog/conditional-maven.png",
     icon: "📦",
     href: "https://www.geeksforgeeks.org/conditional-dependency-management-using-maven-profiles/?itm_source=auth&itm_medium=contributions&itm_campaign=articles",
     publishedAt: "2023-01-01",
@@ -39,6 +39,57 @@ const posts = [
         icon: "🟩",
         type: " geeksforgeeks",
         href: "https://www.geeksforgeeks.org/conditional-dependency-management-using-maven-profiles/?itm_source=auth&itm_medium=contributions&itm_campaign=articles",
+      },
+    ],
+  },
+  {
+    title: "Neon T3 Starter Kit: Supercharging Web Development with Serverless Postgres",
+    thumbnail: "/blog/neon-starter.png",
+    icon: "💻",
+    href: "https://dev.to/chiragagg5k/neon-t3-starter-kit-supercharging-web-development-with-serverless-postgres-13fg",
+    publishedAt: "2024-08-28",
+    tags: ["neon", "typescript", "guide"],
+    description:
+      "A starter kit that uses Neon database and T3 stack to build a full-stack web application with TypeScript.",
+    links: [
+      {
+        icon: "💻",
+        type: " dev.to",
+        href: "https://dev.to/chiragagg5k/neon-t3-starter-kit-supercharging-web-development-with-serverless-postgres-13fg",
+      },
+    ],
+  },
+  {
+    title: "From Kubernetes Chaos to Calm: A Cyclops Adventure",
+    thumbnail: "/blog/cyclops.png",
+    icon: "💻",
+    href: "https://dev.to/chiragagg5k/from-kubernetes-chaos-to-calm-a-cyclops-adventure-1b5m",
+    publishedAt: "2024-07-30",
+    tags: ["kubernetes", "cyclops", "guide"],
+    description:
+      "A guide to understanding and managing Kubernetes Chaos, and how to use it to improve the reliability of your applications.",
+    links: [
+      {
+        icon: "💻",
+        type: " dev.to",
+        href: "https://dev.to/chiragagg5k/from-kubernetes-chaos-to-calm-a-cyclops-adventure-1b5m",
+      },
+    ],
+  },
+  {
+    title: "My Journey in Authorization with OPAL",
+    thumbnail: "/blog/opal.png",
+    icon: "💻",
+    href: "https://dev.to/chiragagg5k/my-journey-in-authorization-with-opal-1072",
+    publishedAt: "2024-06-23",
+    tags: ["opal", "authorization", "guide"],
+    description:
+      "Understanding the basics of authorization and how to implement it using OPAL, a powerful authorization library.",
+    links: [
+      {
+        icon: "💻",
+        type: " dev.to",
+        href: "https://dev.to/chiragagg5k/my-journey-in-authorization-with-opal-1072",
       },
     ],
   },
