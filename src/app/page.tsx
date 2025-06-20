536984cb1c75008b3df716f6817d90e6bb2004f8
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2025-06-20 15:54:27 +0530

    fix: education section from overflowing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -97,7 +97,7 @@ export default function Page() {
           </div>
         </section>
         <section id="education">
-          <div className="flex min-h-0 min-w-[60vw] w-full flex-col gap-y-3">
+          <div className="flex min-h-0 max-w-2x min-w-[800px] w-full flex-col gap-y-3">
             <BlurFade delay={BLUR_FADE_DELAY * 7}>
               <h2 className="text-xl font-bold">Education</h2>
             </BlurFade>
